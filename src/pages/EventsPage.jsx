98237d0ef0c13e523c268cbfe5fd0a3b25d2c248
BARBARA PETERS <barbara.peters@gmail.com>
2025-09-09 09:55:19 +0200

    Load list of events on page

--- src/pages/EventsPage.jsx
+++ src/pages/EventsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
-import { useLoaderData } from "react-router-dom";
+import { Heading, Card, Image, Badge } from "@chakra-ui/react";
+import { useLoaderData, Link } from "react-router-dom";
 
 export const loader = async () => {
   const users = await fetch("http://localhost:3000/users");
@@ -18,5 +18,28 @@ export const EventsPage = () => {
   console.table(users);
   console.table(events);
   console.table(categories);
-  return <Heading>List of events</Heading>;
+
+  return (
+    <>
+      <Heading>List of events</Heading>
+      <Card className="events-list">
+        {events.map((event) => {
+          return (
+            <div key={event.id} className="event">
+              <Link to={`event/${event.id}`}>
+                <h2>{event.title}</h2>
+              </Link>
+              <Image src={event.image} alt={event.title} />
+              <p>{event.description}</p>
+              <p>{event.startTime}</p>
+              <p>{event.endTime}</p>
+              <Badge variant="outline" colorScheme="green">
+                {categories.find((cat) => cat.id === event.categoryId)?.name}
+              </Badge>
+            </div>
+          );
+        })}
+      </Card>
+    </>
+  );
 };
