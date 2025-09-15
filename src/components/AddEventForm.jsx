29c31acec5f8e7ae9660d80c8ae4453399a862a8
BARBARA PETERS <barbara.peters@gmail.com>
2025-09-15 11:43:09 +0200

    console.logs verwijderd uit AddEventForm

--- src/components/AddEventForm.jsx
+++ src/components/AddEventForm.jsx
@@ -21,7 +21,6 @@ export const AddEventForm = ({ onSubmit }) => {
   const [selectedCategories, setSelectedCategories] = useState([]);
 
   const { categories } = useContext(EventContext);
-  console.table(categories); // Debug log
 
   const handleSubmit = (e) => {
     e.preventDefault();
